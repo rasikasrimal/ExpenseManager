54dc0b40c5ba5e63774a2b78ae1a550c015d79dc
rasikasrimal <rasikacooldj620@gmail.com>
2023-09-23 22:36:38 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:expense_manager/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       home: const Expenses(),
     ),
