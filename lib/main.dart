82b07c3463c9d93031594f3e692965d93d94c834
rasikasrimal <rasikacooldj620@gmail.com>
2023-09-27 15:29:58 +0530

    Changed Theme Using A Color Scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:expense_manager/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Colors.red,
+);
+
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.background,
+            foregroundColor: kColorScheme.onBackground,
+          )),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(useMaterial3: true),
       home: const Expenses(),
     ),
   );
