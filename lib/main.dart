b1e12f2ae4f534275a18f048530222302efc8e49
rasikasrimal <rasikacooldj620@gmail.com>
2023-09-27 18:06:00 +0530

    Set theme depend on device's theme (Dark/Light)

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ void main() {
             ),
       ),
       debugShowCheckedModeBanner: false,
-      themeMode: ThemeMode.dark,
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
